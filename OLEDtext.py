0a28b73cc496e3fb712cbc76738ab51798d891ae
Zeph H <65759870+ZephCreations@users.noreply.github.com>
2025-06-21 13:41:08 +1000

    Update OLEDtext.py to use textwrap module

--- OLEDtext.py
+++ OLEDtext.py
@@ -3,24 +3,16 @@ from OLED import OLED
 from luma.core.render import canvas
 from luma.core.virtual import terminal
 
+from textwrap import fill
+
 
 class OLEDtext(OLED):
 
     def __init__(self, device, text="", display_console=False):
         OLED.__init__(self, device)
-
-        # Setup text
-        self.text = text
-        self._add_line_breaks()
-
-        # Other variables
+        self.text = fill(text, 20)
         self.display_console = display_console
 
-    def _add_line_breaks(self, every=20):
-        lines = []
-        for i in range(0, len(self.text), every):
-            lines.append(self.text[i:i + every])
-        self.text = '\n'.join(lines)
 
     def update(self):
         if self.display_console:
