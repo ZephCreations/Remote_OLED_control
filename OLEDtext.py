cc0a3401cecf17d73fb61f0c3bd560c80cf944ab
Zeph H <65759870+ZephCreations@users.noreply.github.com>
2025-06-21 16:59:58 +1000

    Update OLEDtext.py to use textwrap module and preserve newlines

--- OLEDtext.py
+++ OLEDtext.py
@@ -1,16 +1,18 @@
+from click import wrap_text
+
 from OLED import OLED
 
 from luma.core.render import canvas
 from luma.core.virtual import terminal
 
-from textwrap import fill
+from textwrap import wrap
 
 
 class OLEDtext(OLED):
 
     def __init__(self, device, text="", display_console=False):
         OLED.__init__(self, device)
-        self.text = fill(text, 20)
+        self.text = wrap_text(text)
         self.display_console = display_console
 
 
@@ -21,3 +23,16 @@ class OLEDtext(OLED):
         else:
             with canvas(self.device) as draw:
                 draw.text((0, 0), f"{self.text}", fill="white", font_size=12)
+
+
+    @staticmethod
+    def wrap_text(text):
+        lines = []
+
+        for line in text.splitlines():
+            if line.strip != '':
+                wrapped = wrap(line, 20, break_long_words=False, replace_whitespace=False)
+                lines.append('\n'.join(wrapped))
+
+        return '\n'.join(lines)
+
