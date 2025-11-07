4e305647b9e3473fd66989241652acc36d7f8673
Zeph H <65759870+ZephCreations@users.noreply.github.com>
2025-11-07 22:01:19 +1000

    Updated startWebServer.py to use updated Database objects

--- src/Website/startWebServer.py
+++ src/Website/startWebServer.py
@@ -15,45 +15,43 @@ def initialise_database(no_screens):
         screen = Screen(screen_no)
         # If address isn't added, add it
         get_value = screen_dao.get_screen_by_value(screen_no)
-        print(get_value)
-        if len(get_value) == 0:
-            screen_dao.add_screen(screen)
+        if get_value is None:
+            screen = screen_dao.add_screen(screen)
             print(f"Add item {screen_no}")
         else:
-            screen.id = get_value[0][0]
+            screen = get_value
         screen_list.append(screen)
+    print(screen_list)
 
     # Add default profile.
     # TODO: Add restore to previous settings (like previous profiles etc.)
     profile_dao = ProfileDAO()
     profile = Profile("Default")
     get_value = profile_dao.get_profile_by_value("Default")
-    if len(get_value) == 0:
-        profile_dao.add_profile(profile)
+    if get_value is None:
+        profile = profile_dao.add_profile(profile)
     else:
-        profile.id = get_value[0][0]
+        profile = get_value
 
     # Add default displays (TEXT type)
     type_dao = DispTypeDAO()
-    display_type = DispType(DispTypeList.TEXT.name)
-    get_value = type_dao.get_type_by_value(DispTypeList.TEXT.name)
-    display_type.id = get_value[0][0]
+    display_type = type_dao.get_type_by_value(DispTypeList.TEXT.name)
     display_dao = DisplayDAO()
     for screen_no in range(1, no_screens + 1):
-        display = Display(profile.id, screen_list[screen_no-1].id, display_type.id, "Blank")
+        display = Display(profile.id, screen_list[screen_no-1].id, display_type.id, "")
         # If display isn't added, add it
-        get_value = display_dao.get_display_by_value(display.profile_id, display.screen_id, display.type_id)
-        if len(get_value) == 0:
+        display_get = display_dao.get_display_by_value(display.profile_id, display.screen_id, display.type_id)
+        if display_get is None:
             display_dao.add_display(display)
             print(f"Add display {display.id}")
         else:
-            display.id = get_value[0][0]
+            display = display_get
 
 
 
 
 if __name__ == "__main__":
     initialise_database(6)
-    # server = ThreadingHTTPServer(Address, WebRequestHandler)
-    # print(f"Server running on address {Address}")
-    # server.serve_forever()
+    server = ThreadingHTTPServer(Address, WebRequestHandler)
+    print(f"Server running on address {Address}")
+    server.serve_forever()
