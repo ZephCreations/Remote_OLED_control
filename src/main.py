fbed4da46189517ad5943def58c89d53e7f4cb1a
Zeph H <65759870+ZephCreations@users.noreply.github.com>
2025-11-18 16:00:33 +1000

    Updated main to include database initialisation

--- src/main.py
+++ src/main.py
@@ -3,10 +3,52 @@ import threading
 
 from Website import WebRequestHandler
 from http.server import ThreadingHTTPServer
+from Database import *
 
 from OLED import OLEDthread
 from OLED import OLEDtext
 
+
+def initialise_database(no_screens):
+
+    # Add screens
+    screen_dao = ScreenDAO()
+    screen_list = []
+    for screen_no in range(1, no_screens + 1):
+        screen = Screen(screen_no)
+        # If address isn't added, add it
+        get_value = screen_dao.get_screen_by_value(screen_no)
+        if get_value is None:
+            screen = screen_dao.add_screen(screen)
+            print(f"Add item {screen_no}")
+        else:
+            screen = get_value
+        screen_list.append(screen)
+    print(screen_list)
+
+    # Add default profile.
+    # TODO: Add restore to previous settings (like previous profiles etc.)
+    profile_dao = ProfileDAO()
+    profile = Profile("Default")
+    get_value = profile_dao.get_profile_by_value("Default")
+    if get_value is None:
+        profile = profile_dao.add_profile(profile)
+    else:
+        profile = get_value
+
+    # Add default displays (TEXT type)
+    type_dao = DispTypeDAO()
+    display_type = type_dao.get_type_by_value(DispTypeList.TEXT.name)
+    display_dao = DisplayDAO()
+    for screen_no in range(1, no_screens + 1):
+        display = Display(profile.id, screen_list[screen_no-1].id, display_type.id, "")
+        # If display isn't added, add it
+        display_get = display_dao.get_display_by_value(display.profile_id, display.screen_id)
+        if display_get is None:
+            display_dao.add_display(display)
+            print(f"Add display {display.id}")
+        else:
+            display = display_get
 
 def setup_oleds():
     # TODO: Reset to previous values
@@ -35,6 +77,7 @@ if __name__ == "__main__":
     num_screens = 6
     setup_threads(num_screens, 2, multiplexer_address)
     setup_oleds()
+    initialise_database(num_screens)
 
     stop_event = threading.Event()
     oled_port_lock = threading.Lock()
